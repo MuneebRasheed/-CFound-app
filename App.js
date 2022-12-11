b9cc7266acf6d0040beda1b338e1946f3451d50e
Muhammad Junaid Nadeem <junaidnadeem266@gmail.com>
2022-12-12 00:01:21 +0500

    Checking

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Home';
+import Events from './Events';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={Home}/>
+
+      <Stack.Screen name="Home" component={Home}/>
+      <Stack.Screen name="Events" component={Events}/> 
+  
+        
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
