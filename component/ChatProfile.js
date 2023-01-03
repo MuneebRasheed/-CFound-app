5173d13e6cafadce11f21d27c3eefd2678eeb233
Muneeb <ahsan@vaionex.com>
2023-01-03 14:03:04 +0500

    chatmodule

--- component/ChatProfile.js
+++ component/ChatProfile.js
@@ -1,17 +1,17 @@
 import { StyleSheet, Text, TextInput, View, Image, TouchableOpacity, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 
-const ChatProfile = () => {
+const ChatProfile = ({ name, id, navigation }) => {
     return (
-        <TouchableOpacity style={{ alignItems: 'center' }}>
+        <View style={{ alignItems: 'center' }} >
             <View style={{ flexDirection: 'row', width: '90%', backgroundColor: 'white', paddingRight: 19 }}>
                 <Image style={{ width: 50, height: 50, borderRadius: 100, marginTop: '4%', marginRight: '4%', marginLeft: '4%', color: '#2192FF' }}
                     source={require('../assets/profile.jpeg')} />
                 {/* onPress={()=>navigation.navigate('Chat', {item} )}  */}
-                <TouchableOpacity style={{ flexDirection: 'row', width: '80%', justifyContent: 'space-between', height: 70, marginTop: '3%' }}
+                <View style={{ flexDirection: 'row', width: '80%', justifyContent: 'space-between', height: 70, marginTop: '3%' }}
                 >
                     <View>
-                        <Text style={{ fontSize: 18, color: 'black', fontWeight: 'bold', marginBottom: '2%' }}>{"Laiba"}</Text>
+                        <Text style={{ fontSize: 18, color: 'black', fontWeight: 'bold', marginBottom: '2%' }}>{name}</Text>
                         <Text style={{ fontSize: 16, color: 'black' }}>{"Tang Na karo"}</Text>
                     </View>
                     <View style={{ alignItems: 'flex-end', right: '15%' }}>
@@ -20,9 +20,9 @@ const ChatProfile = () => {
                             <Text style={{ fontSize: 15, color: 'white' }}>{"3"}</Text>
                         </View>
                     </View>
-                </TouchableOpacity>
+                </View>
             </View>
-        </TouchableOpacity>
+        </View>
     )
 }
 
--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -8,26 +8,29 @@ import Lost from '../Screens/Lost';
 import Found from '../Screens/Found';
 import BottomTabNavigator from './TabNavigator';
 import Announcements from '../Screens/Announcements';
+import Chatting from '../Screens/Chatting';
+
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
-    return (
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Home" component={BottomTabNavigator} />
-        <Stack.Screen name="LostItems" component={LostItems} />
-        <Stack.Screen name="Events" component={Events} />
-        <Stack.Screen name="Announcements" component={Announcements} />
-        <Stack.Screen name="Lost" component={Lost}/>
-        <Stack.Screen name="Found" component={Found} options={{ animation: 'none' }}/>
-        <Stack.Screen name="Chat" component={BottomTabNavigator} />
-        <Stack.Screen name="Profile" component={BottomTabNavigator}/> 
-      </Stack.Navigator>
-    );
-  }
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Screen name="Home" component={BottomTabNavigator} />
+      <Stack.Screen name="LostItems" component={LostItems} />
+      <Stack.Screen name="Events" component={Events} />
+      <Stack.Screen name="Chat" component={BottomTabNavigator} />
+      <Stack.Screen name="Chatting" component={Chatting} />
+      <Stack.Screen name="Announcements" component={Announcements} />
+      <Stack.Screen name="Lost" component={Lost} />
+      <Stack.Screen name="Found" component={Found} options={{ animation: 'none' }} />
+      <Stack.Screen name="Profile" component={BottomTabNavigator} />
+    </Stack.Navigator>
+  );
+}
 
-  
-  
+
+
 export default StackNavigator;
